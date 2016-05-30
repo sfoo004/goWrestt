17e2dd0d9862381009183edeff78d4b4e5ec6dfe
Steve Foo <stevefoo13@gmail.com>
2016-05-29 20:59:43 -0400

    Added SQL ability to the server

--- src/main/controller/NewsController.go
+++ src/main/controller/NewsController.go
@@ -3,50 +3,165 @@ package controller
 import (
 	"net/http"
 	"github.com/gorilla/mux"
-
+	"github.com/go-sql-driver/mysql"
 	"io/ioutil"
+	"database/sql"
+	"fmt"
+	"encoding/json"
+	"strconv"
 )
 
-func GetAllNews(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Hello world"))
+type news struct {
+	Id int `json:"id"`
+	CreatedAt float32 `json:"createdAt"`
+	Title string `json:"title"`
+	Body string `json:"body"`
+}
+
+func GetAllNews(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Hello world"))
 	//send request to database
+	db, e := sql.Open("mysql", "username:password@tcp(localhost:3306)/news")
+	if( e != nil){
+		fmt.Print(e)
+	}
+
+	//set mime type to JSON
+	res.Header().Set("Content-type", "application/json")
+
+	err := req.ParseForm()
+	if err != nil {
+		http.Error(res, fmt.Sprintf("error parsing url %v", err), 500)
+	}
+
+	//can't define dynamic slice in golang
+	var result = make([]string,1000)
+
+	st, err := db.Prepare("SELECT * FROM news")
+	if err != nil{
+		fmt.Print( err );
+	}
+	rows, err := st.Query()
+	if err != nil {
+		fmt.Print( err )
+	}
+	i := 0
+	for rows.Next() {
+		var id int
+		var createdAt float32
+		var title string
+		var body string
+		err = rows.Scan( &id, &createdAt, &title, &body )
+		new := news{Id:id, CreatedAt:createdAt, Title:title, Body:body}
+		b, err := json.Marshal(new)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		result[i] = fmt.Sprintf("%s", string(b))
+		i++
+	}
+	result = result[:i]
+
+	json, err := json.Marshal(result)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Send the text diagnostics to the client.
+	fmt.Fprintf(res,"%v",string(json))
+	//fmt.Fprintf(response, " request.URL.Path   '%v'\n", request.Method)
+	db.Close()
 
 	//retrieve results from DB. Parse to JSON to send back
 }
 
-func GetNews(w http.ResponseWriter, req *http.Request) {
+func GetNews(res http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
-	w.Write([]byte(vars["id"]))
-}
-func PostNews(w http.ResponseWriter, req *http.Request){
-	w.Write([]byte("Welcome to HELLo"))
+	res.Write([]byte(vars["id"]))
+	//send request to database
+	db, e := sql.Open("mysql", "username:password@tcp(localhost:3306)/news")
+	if( e != nil){
+		fmt.Print(e)
+	}
+
+	//set mime type to JSON
+	res.Header().Set("Content-type", "application/json")
+
 	err := req.ParseForm()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		http.Error(res, fmt.Sprintf("error parsing url %v", err), 500)
 	}
-	body, err := ioutil.ReadAll(req.Body)
+
+	//can't define dynamic slice in golang
+	var result = make([]string,1000)
+
+	st, err := db.Prepare("SELECT * FROM news WHERE id= ? ")
+	if err != nil{
+		fmt.Print( err );
+	}
+	rows, err := st.Query()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Print( err )
+	}
+	i := 0
+	for rows.Next() {
+		var id int
+		var createdAt float32
+		var title string
+		var body string
+		err = rows.Scan( &id, &createdAt, &title, &body )
+		new := news{Id:id, CreatedAt:createdAt, Title:title, Body:body}
+		b, err := json.Marshal(new)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		result[i] = fmt.Sprintf("%s", string(b))
+		i++
+	}
+	result = result[:i]
+
+	json, err := json.Marshal(result)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
+
+	// Send the text diagnostics to the client.
+	fmt.Fprintf(res,"%v",string(json))
+	//fmt.Fprintf(response, " request.URL.Path   '%v'\n", request.Method)
+	db.Close()
+}
+
+func PostNews(res http.ResponseWriter, req *http.Request){
+	res.Write([]byte("Welcome to HELLo"))
+	err := req.ParseForm()
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var n news
+	decoder := json.NewDecoder(req.Body).Decode(&n)
+
 
 	//validate input
 	//send request to database
 
 	//retireve results from DB. Parse to JSON to send back
 }
-func PutNews(w http.ResponseWriter, req *http.Request) {
+func PutNews(res http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
-	w.Write([]byte(vars["id"]))
+	res.Write([]byte(vars["id"]))
 	err := req.ParseForm()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	//validate input
@@ -56,9 +171,9 @@ func PutNews(w http.ResponseWriter, req *http.Request) {
 	//retrieve results from DB. Parse to JSON to send back
 }
 
-func DeleteNews(w http.ResponseWriter, req *http.Request) {
+func DeleteNews(res http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
-	w.Write([]byte(vars["id"]))
+	res.Write([]byte(vars["id"]))
 	//send request to database
 
 	//retrieve results from DB. Parse to JSON to send back
